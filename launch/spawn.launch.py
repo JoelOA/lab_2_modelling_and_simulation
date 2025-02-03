0034f0c0fb4b368a7c683d63bb435392517ff5da
JoelOA <joeloseifiifi@gmail.com>
2025-02-03 16:17:20 +0000

    initial commit

---
+++ launch/spawn.launch.py
@@ -0,0 +1,83 @@
+from os.path import join
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchContext, LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+PACKAGE_NAME = "turtle_bot_description"
+
+def launch_setup(context: LaunchContext) -> list:
+    pkg_share = FindPackageShare(package=PACKAGE_NAME).find(PACKAGE_NAME)
+    print(pkg_share)
+
+    world = LaunchConfiguration("world").perform(context)
+    print(f"World to load: {world}")
+    
+    #Spawn robot at the given position
+    x = LaunchConfiguration("x").perform(context)
+    y = LaunchConfiguration("y").perform(context)
+    z = LaunchConfiguration("z").perform(context)
+    roll = LaunchConfiguration("roll").perform(context)
+    pitch = LaunchConfiguration("pitch").perform(context)
+    yaw = LaunchConfiguration("yaw").perform(context)
+
+    # Gazebo launch file
+    world_filepath = join(pkg_share, "worlds", f"{world}.world")
+    print(world_filepath)
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                join(
+                    get_package_share_directory("ros_gz_sim"),
+                    "launch",
+                    "gz_sim.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={
+                "gz_args": ["-r -v4 ", world_filepath],
+                "on_exit_shutdown": "true",
+        }.items(),
+    )
+
+    # spawn the robot in Gazebo
+    spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-topic", "robot_description",
+            "-name", "turtle_bot",
+            "-x", str(x),
+            "-y", str(y),
+            "-z", str(z),
+            "-R", str(roll),
+            "-P", str(pitch),
+            "-Y", str(yaw),
+        ],
+        output="screen",
+    )
+
+    return [gazebo, spawn_entity]
+
+def generate_launch_description() -> LaunchDescription:
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument("x", default_value="0", description="X position"),
+            DeclareLaunchArgument("y", default_value="0", description="Y position"),
+            DeclareLaunchArgument("z", default_value="0.056", description="Z position"),
+            DeclareLaunchArgument("roll", default_value="0", description="Roll"),
+            DeclareLaunchArgument("pitch", default_value="0", description="Pitch"),
+            DeclareLaunchArgument("yaw", default_value="0", description="Yaw"),
+            DeclareLaunchArgument("world", default_value="simple", description="World name"),
+            OpaqueFunction(function=launch_setup)
+        ]
+    )
